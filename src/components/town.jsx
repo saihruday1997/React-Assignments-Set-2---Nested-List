2705e058fc83c7512416486a307bc7c45a35c026
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-12-16 15:57:11 +0530

    chngs

--- src/components/town.jsx
+++ src/components/town.jsx
@@ -4,7 +4,5 @@ export default function Town(props) {
   let town = props.town;
   let id = props.index;
 
-  let [clicked, setClicked] = useState(false);
-
   return <div id={`town${id}`}>{town.name}</div>;
 }
